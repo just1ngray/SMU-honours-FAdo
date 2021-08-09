941e21ee21cc9556234e376309aa7c2e49053406
just1ngray <just1ngray@outlook.com>
2021-08-09 12:43:43 -0300

    move test fadoize

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,36 +1,5 @@
 import unittest
 
-from benchmark import sample
-
-
-class TestFAdoize(unittest.TestCase):
-    def test_charclass(self):
-        f = sample.FAdoize
-        self.assertEqual(f('\\d'), '[0-9]')
-        self.assertEqual(f('\\D'), '[^0-9]')
-        self.assertEqual(f('[\\d]'), '[0-9]')
-
-        self.assertEqual(f('\\s'), ' ')
-        self.assertEqual(f('\\S'), '[^ ]')
-        self.assertEqual(f('[\\s]'), '[ ]')
-
-        self.assertEqual(f('\\w'), '[0-9A-Za-z_]')
-        self.assertEqual(f('\\W'), '[^0-9A-Za-z_]')
-        self.assertEqual(f('[\\w]'), '[0-9A-Za-z_]')
-
-    def test_almost_charclass(self):
-        f = sample.FAdoize
-        self.assertEqual(f('\\\\d'), '(\\\\ d)')
-        self.assertEqual(f('\\\\D'), '(\\\\ D)')
-        self.assertEqual(f('[\\\\d]'), '[\\\\d]')
-
-        self.assertEqual(f('\\\\s'), '(\\\\ s)')
-        self.assertEqual(f('\\\\S'), '(\\\\ S)')
-        self.assertEqual(f('[\\\\s]'), '[\\\\s]')
-
-        self.assertEqual(f('\\\\w'), '(\\\\ w)')
-        self.assertEqual(f('\\\\W'), '(\\\\ W)')
-        self.assertEqual(f('[\\\\w]'), '[\\\\w]')
 
 
 
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,37 @@
 import unittest
 
-from benchmark.util import RangeList
+from benchmark.util import RangeList, FAdoize
+
+class TestFAdoize(unittest.TestCase):
+    # Also tested in `test_convert.py` indirectly
+    def test_charclass(self):
+        f = FAdoize
+        self.assertEqual(f('\\d'), '[0-9]')
+        self.assertEqual(f('\\D'), '[^0-9]')
+        self.assertEqual(f('[\\d]'), '[0-9]')
+
+        self.assertEqual(f('\\s'), ' ')
+        self.assertEqual(f('\\S'), '[^ ]')
+        self.assertEqual(f('[\\s]'), '[ ]')
+
+        self.assertEqual(f('\\w'), '[0-9A-Za-z_]')
+        self.assertEqual(f('\\W'), '[^0-9A-Za-z_]')
+        self.assertEqual(f('[\\w]'), '[0-9A-Za-z_]')
+
+    def test_almost_charclass(self):
+        f = FAdoize
+        self.assertEqual(f('\\\\d'), '(\\\\ d)')
+        self.assertEqual(f('\\\\D'), '(\\\\ D)')
+        self.assertEqual(f('[\\\\d]'), '[\\\\d]')
+
+        self.assertEqual(f('\\\\s'), '(\\\\ s)')
+        self.assertEqual(f('\\\\S'), '(\\\\ S)')
+        self.assertEqual(f('[\\\\s]'), '[\\\\s]')
+
+        self.assertEqual(f('\\\\w'), '(\\\\ w)')
+        self.assertEqual(f('\\\\W'), '(\\\\ W)')
+        self.assertEqual(f('[\\\\w]'), '[\\\\w]')
+
 
 class TestRangeList(unittest.TestCase):
     @classmethod
