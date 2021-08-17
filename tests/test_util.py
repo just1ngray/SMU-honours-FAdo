be211573b0a2369ac7a77d31457851177ebe92f0
just1ngray <just1ngray@outlook.com>
2021-08-17 14:20:27 -0300

    increase sample size

--- tests/test_util.py
+++ tests/test_util.py
@@ -179,7 +179,7 @@ class TestRangeList(unittest.TestCase):
 class TestWeightedRandomItem(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.samplesize = 10000.0
+        cls.samplesize = 15000.0
         cls.iterations = range(int(cls.samplesize))
         cls.threshold = 0.01
 
