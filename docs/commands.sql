8d193b1dca707c64e3742aeabc22c9ce84a16f4d
just1ngray <just1ngray@outlook.com>
2021-08-20 14:22:08 -0300

    reflect col name changes

--- docs/commands.sql
+++ docs/commands.sql
@@ -1,16 +1,16 @@
 -- Select distinct non-error re rows
 SELECT *
 FROM (
-	SELECT *, ROW_NUMBER() OVER(PARTITION BY re ORDER BY url ASC, lineNum ASC) rn
+	SELECT *, ROW_NUMBER() OVER(PARTITION BY re_math ORDER BY url ASC, lineNum ASC) rn
 	FROM expressions
-	WHERE re NOT LIKE '%Error%'
+	WHERE re_math NOT LIKE '%Error%'
 ) a
 WHERE rn=1
-ORDER BY re ASC;
+ORDER BY re_math ASC;
 
 
 -- Count distinct non-error re rows by language
-SELECT lang, count(DISTINCT re)
+SELECT lang, count(DISTINCT re_math)
 FROM expressions
-WHERE re NOT LIKE '%Error%'
+WHERE re_math NOT LIKE '%Error%'
 GROUP BY lang;
